1ed4264262bfafdd046ba24bf280b51482b31c4e
Bartosz Leśniewski <blesniewski@outlook.com>
2023-11-05 03:23:24 +0100

    Remove unused usings

--- GameOfLife/GameState.cs
+++ GameOfLife/GameState.cs
@@ -1,14 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics.Metrics;
-using System.Linq;
 using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Windows.Controls;
-using System.Windows.Media;
-using static System.Net.Mime.MediaTypeNames;
-using System.Windows.Shapes;
 using static GameOfLife.Enums;
 
 namespace GameOfLife
